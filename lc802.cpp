0cfa1aa3b552008befe7041d5167e90d8b10adcc
Rikul patel <rikulpatel@gmail.com>
2025-02-05 19:50:45 -0600

    start lc802

--- lc802.cpp
+++ lc802.cpp
@@ -1,3 +1,9 @@
+#include <vector>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
 class Solution {
 public:
     /*
@@ -12,7 +18,6 @@ public:
     [[],[0,2,3,4],[3],[4],[1],[]]
 
     2 -> [0,2,3,4]
-
     0,2,3,4
     2,3,4
     3,4,3
@@ -20,8 +25,7 @@ public:
     3,4,4
     4,4,1
 
-    2 ->  [3]
-    
+    2 ->  [3]    
     3
     4
     1
@@ -31,22 +35,52 @@ public:
     4.3.1
     3,1,0,2,3,4
 
-
     */
+
+   // NOT IMPLEMENTED
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> safeNodes;
+        std::set<int> safeNodes;
 
         for(int i = 0; i < graph.size(); i++) {
-            if (graph[i].size() == 0) { 
-                safeNodes.push_back(i);
+            std::set<int> visited;
+
+            if (graph[i].size() == 0 || (graph[i].size() == 1 && graph[i][0] == i)) { 
+                safeNodes.insert(i);
                 continue;
             }
 
-            vector<int> nodes = graph[i];
+            vector<int> q = graph[i];
+            bool isSafe = true;
+            while (q.size() > 0) {
+                int node = q.front();
+                q.erase(q.begin());
+                
+                if (safeNodes.find(node) != safeNodes.end()) {
+                    continue;
+                }   
 
+                // its empty or points to self.
+                if (graph[node].size() == 0) {
+                    safeNodes.insert(node);
+                    continue;
+                }
+            
+                if (visited.find(node) != visited.end()) {
+                    cout << "Cycle detected at " << node << endl;
+                    isSafe = false;
+                    break;
+                }
+                
+                visited.insert(node);
+                q.insert(q.end(), graph[node].begin(), graph[node].end());
+            }
+
+            if (isSafe) {
+                safeNodes.insert(i);
+            }
         }
 
 
-        return vector<int>();
+        return vector<int>(safeNodes.begin(), safeNodes.end());
     }
 };
