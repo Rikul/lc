1a3ae2043d547935f8206c84357112313514fcdd
Rikul patel <rikulpatel@gmail.com>
2025-04-05 13:38:43 -0500

    fixed lc2874

--- lc2874.cpp
+++ lc2874.cpp
@@ -5,22 +5,35 @@
 class Solution {
 public:
     long long maximumTripletValue(std::vector<int>& nums) {
-        std::vector<int> prefix_max, suffix_max;
-        int max_temp1 = 0, max_temp2 = 0;
+        std::vector<int> prefix_max;
+        std::vector<int>suffix_max;
+        long long max_temp1 = 0, max_temp2 = 0;
+
+        prefix_max.resize(nums.size());
+        suffix_max.resize(nums.size());
+
         for(int i=0, j = nums.size() - 1; i < nums.size() && j >= 0; i++, j--) {
             if (nums[i] > max_temp1)
                 max_temp1 = nums[i];
+
             if (nums[j] > max_temp2)
                 max_temp2 = nums[j];
             
-            suffix_max.insert(suffix_max.begin(), max_temp2);
-            prefix_max.push_back(max_temp1);
+            prefix_max[i] = max_temp1;
+            suffix_max[j] = max_temp2;
         }
 
+        max_temp1 = 0;
+        for(int i=1; i < nums.size() - 1; i++) {
+            max_temp2 = ((long)prefix_max[i-1] - (long)nums[i]) * suffix_max[i+1];
+            if (max_temp2 > max_temp1) 
+                max_temp1 = max_temp2;
+        } 
 
-        return 0;
+        return max_temp1;
     }
 };
+
 
 int main() {
     Solution sol;
