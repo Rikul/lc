cd66c52c751019d6c6cb33c9441662b69040ec59
Rikul patel <rikulpatel@gmail.com>
2025-08-25 22:32:27 -0500

    lc40

--- lc40.py
+++ lc40.py
@@ -3,38 +3,33 @@ from typing import List
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 
-        candidates.sort()  # Sort to handle duplicates and for easier combination generation
-
         matches = []
-        memo = {}
+        candidates.sort()
 
         def backtrack(curr : List[int], start_idx : int, curr_sum : int):
-
+                        
             if curr_sum == target:
-                #curr.sort()
+
                 if curr not in matches:
-                    matches.append(curr)
+                    matches.append(curr[:])
+                else:
+                    print(curr)
 
                 return
             
-            #print(repr(curr))    
-
             for i in range(start_idx, len(candidates)):
                 n = candidates[i]
                 if curr_sum + n <= target:
                     backtrack(curr + [n], i+1, curr_sum + n)
-                else:
-                    break  # No need to continue if the sum exceeds target
 
+        
         backtrack([],0 ,0)
 
         return matches
+    
 
 if __name__ == "__main__":
     sol = Solution()
-
-    candiates = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,33,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,44,4,4,4,5,5,5,5,5,5,5,5,5,5,5,49,5,5,5,5,6,6,6,6]
-    #candiates = [1,1,2,3,4,5]
-    target = 29
-
-    print(sol.combinationSum2(candiates, target))
+    result = sol.combinationSum2([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5],15)
+    print(result)
+    
