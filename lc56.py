124192fba1c13d190d53cc506891ed585577ea68
Rikul patel <rikulpatel@gmail.com>
2025-09-11 15:09:39 -0500

    added #56

---
+++ lc56.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort()        
+
+        merged = [intervals[0]]
+        for i in range(1, len(intervals)):
+            prev_start, prev_end = merged[-1]
+
+            if intervals[i][0] >= prev_start and intervals[i][1] <= prev_end:
+                continue 
+            elif intervals[i][0] > prev_end:
+                merged.append(intervals[i])
+            elif intervals[i][0] >= prev_start and intervals[i][1] > prev_end:
+                merged[-1] = [prev_start, intervals[i][1]] 
+
+        return merged
+        
